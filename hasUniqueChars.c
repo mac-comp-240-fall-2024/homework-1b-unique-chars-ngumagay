4d9cac11542044211b63e8f2161f4ca3a68db4d6
ngumagay <ngumagay@mscs2024.olri.macalester.edu>
2024-09-26 16:31:01 +0000

    done

--- hasUniqueChars.c
+++ hasUniqueChars.c
@@ -1,115 +1,101 @@
 /*
  * hasUniqueChars.c
- * 
- * TODO: replace this line with lines containing a description
- * 
- * Author: 
+ *
+ * Two helper functions that support UniqueChars function as well as the UniqueChars function.
+ *
+ * Author: Nathan Gumagay
  */
 
-#include <stdio.h>  // fprintf, printf
-#include <stdlib.h> // exit() defined here
-#include <string.h> // useful string operations
-#include <ctype.h>  //useful character operations
-#include <stdbool.h>  // to define a boolean type and true, false
-                      // see https://en.wikibooks.org/wiki/C_Programming/stdbool.h
+#include <stdio.h>   // fprintf, printf
+#include <stdlib.h>  // exit() defined here
+#include <string.h>  // useful string operations
+#include <ctype.h>   //useful character operations
+#include <stdbool.h> // to define a boolean type and true, false
+                     // see https://en.wikibooks.org/wiki/C_Programming/stdbool.h
 
 #include "binary_convert.h"
-
 
 /*
  * This function is for debugging by printing out the value
  * of an input insigned long as a binary string.
  */
-void seeBits(unsigned long value, char *debug_text) {
-  
-  // string to represent binary value of variable value
+void seeBits(unsigned long value, char *debug_text)
+{
+
   char *bin_str;
 
   bin_str = ulong_to_bin_str(value);
   printf("%s%s\n", debug_text, bin_str);
   free(bin_str);
-  
 }
 
-
-// TODO: Read this carefully to see how to loop over characters of a string
-// TODO: (Remove TODOs once you have completed the task they describe)
 /*
  * Given an input string of chars, check for any non-printing
  * characters and print an error and exit if the string has any.
  */
-void checkInvalid(char * inputStr) {
+void checkInvalid(char *inputStr)
+{
   char nextChar;
   int i;
-  
-  for(i = 0; i < strlen(inputStr); i++) {
+
+  for (i = 0; i < strlen(inputStr); i++)
+  {
     nextChar = inputStr[i];
 
     // if nextChar is invalid (31 or less or 127 as an ascii char code), then bail out
-    if ((nextChar <= 31 ) || (nextChar == 127)) {
+    if ((nextChar <= 31) || (nextChar == 127))
+    {
       fprintf(stderr, "invalid character in string\n");
       exit(EXIT_FAILURE);
     }
   }
 }
 
-
 /*
- * TODO: Replace this code by a good description this function takes in, does and returns.
- * Include the error conditions that cause it to exit with failure.
+ * The function takes in a pointer to a char which is a string, then checking each character of the string to
+ * see if the string has all it's characters unique.
+ * Error conditions: Duplicates, as well as \0,\t,\n all push the function to exit, as well as others in the ascii table that cannot be checked.
  */
-bool hasUniqueChars(char * inputStr) {
-  // bail out quickly if any invalid characters
+bool hasUniqueChars(char *inputStr)
+{
+
   checkInvalid(inputStr);
-  
-  int i;   // loop counter
-  
-  // unsigned long can handle 64 different chars in a string
-  // if a bit at a position is 1, then we have seen that character
-  unsigned long checkBitsA_z = 0;   // for checking A through z and {|}~
-  unsigned long checkBitsexcl_amp =0;  // for checking ! though @ 
 
-  char nextChar;         // next character in string to check
+  int i;
 
-  // -------------------------------------------------------------
-  // This section contains code to display the initial values of checkBitsA_z
-  // and checkBitsexcl_amp, for debugging purposes. 
-  // It also illustrates how to use the seeBits function for debugging.
-  // Printed values should initially be all zeros
-  // TODO: remove or comment out this code when satisfied of function correctness
-  
-  char debug_str_A_z[128];
-  strcpy(debug_str_A_z, "checkBitsA_z before: \n");
-  seeBits(checkBitsA_z, debug_str_A_z);
-  
-  char debug_str_excl_amp[128];
-  strcpy(debug_str_excl_amp, "checkBitsexcl_amp before: \n");
-  seeBits(checkBitsexcl_amp, debug_str_excl_amp);
-  // -------------------------------------------------------------
+  unsigned long checkBitsA_z = 0;      // for checking A through z and {|}~
+  unsigned long checkBitsexcl_amp = 0; // for checking ! though @
 
-  // TODO: Declare additional variables you need here
+  char nextChar;
 
-  
-  for(i = 0; i < strlen(inputStr); i++) {
+  unsigned long mask = 0;
+
+  for (i = 0; i < strlen(inputStr); i++)
+  {
     nextChar = inputStr[i];
-    // TODO: Add your code here to check nextChar, see if it is a duplicate, and update the checkBits variables
-
-    // -------------------------------------------------------------
-    // Below this are examples of debugging print statements you could use
-    // Move/use as makes sense for you!
-    // Modify to work on checkBitsexcl_amp
-    // TODO: Comment out or remove when your function works correctly
-    printf("nextchar int value: %d\n", nextChar);
-    char char_str[2] = "\0";
-    char_str[0] = nextChar;
-    strcpy(debug_str_A_z, "nextchar: ");
-    strcat(debug_str_A_z, char_str);
-    strcat(debug_str_A_z,", checkBitsA_z: \n");
-    seeBits(checkBitsA_z, debug_str_A_z);
-    // ------------------------------------------------------------- 
+    if (nextChar > 64)
+    {
+      mask = 1L << nextChar;
+      if (checkBitsA_z & mask)
+      {
+        return false;
+      }
+      checkBitsA_z |= mask;
+    }
+    else
+    {
+      if (nextChar >= 33 && nextChar <= 64)
+      {
+        mask = 1L << nextChar;
+        if (checkBitsexcl_amp & mask)
+        {
+          return false;
+        }
+        checkBitsexcl_amp |= mask;
+      }
+    }
   }
 
   // if through all the characters, then no duplicates found
   return true;
-  
 }
--- test.c
+++ test.c
@@ -1,66 +1,115 @@
 /*
- * TODO: Add an appropriate descriptive comment here
+ * Tests for UniqueChars function.
  */
 
-#include <stdio.h>    // printf
-#include <stdbool.h>  // to define a boolean type and true, false
-#include <string.h>   // strcpy  to copy a string
+#include <stdio.h>   // printf
+#include <stdbool.h> // to define a boolean type and true, false
+#include <string.h>  // strcpy  to copy a string
 #include <assert.h>
 
-
 // declaration of function from hasUniqueChars.c
-bool hasUniqueChars(char * inputStr);
+bool hasUniqueChars(char *inputStr);
 
-
-int main() {
+int main()
+{
 
   // ------------------------------------------------------------
   // Examples of declaring string constants
   /* Example 1 */
-  char string1[] = "A string declared as an array.\n";
+  char string1[] = "A string declared as an array.";
 
   /* Example 2 */
-  char *string2 = "A string declared as a pointer.\n";
+  char *string2 = "A string declared as a pointer.";
 
   /* Example 3 */
-  char string3[128];   // can copy in up to 127 characters
-                       // chose 128 because it is a multiple of 8 bytes
+  char string3[128]; // can copy in up to 127 characters
+                     // chose 128 because it is a multiple of 8 bytes
   strcpy(string3, "A string constant copied in.\n");
 
   // You can uncomment these to see how the strings were initialized above.
-  // Then you can comment these out again once you see that the 
+  // Then you can comment these out again once you see that the
   // strings were created correctly
-  // printf ("%s", string1);
-  // printf ("%s", string2);
-  // printf ("%s", string3);
+  // printf("%s", string1);
+  // printf("%s", string2);
+  // printf("%s", string3);
 
   // -------------------------------------------------------------
-  // Thorough and exhaustive tests of hasUniqueChars()  
-  bool ok;    // holds the result of hasUniqueChars(); used in asserts
-  
+  // Thorough and exhaustive tests of hasUniqueChars()
+  bool ok; // holds the result of hasUniqueChars(); used in asserts
+
   // Test 1: a test case that should return false because characters are duplicated
-  strcpy(string3, "This should fail (l and s and i and h)");
-  ok = hasUniqueChars(string3);
-  assert(!(ok));
-  
+  // strcpy(string3, "This should fail (l and s and i and h)");
+  // ok = hasUniqueChars(string3);
+  // assert(!(ok));
+
   // Test 2: This should be true and not fail, but won't work until you
   // complete the hasUniqueChars() function
-  strcpy(string3, "abcdefghij");
+  // strcpy(string3, "abcdefghij");
+  // ok = hasUniqueChars(string3);
+  // assert(ok);
+
+  /*
+   * Test that should pass, string with all unique characters being checked by checkBitsA_z.
+   */
+  strcpy(string1, "AcolkmN");
+  ok = hasUniqueChars(string1);
+  assert(ok);
+
+  /*
+   * Test that should fail, string with one unique characters being checked by checkBitsA_z.
+   */
+  strcpy(string1, "AnAmm");
+  ok = hasUniqueChars(string1);
+  assert(!ok);
+
+  /*
+   * Test that should pass, string with capital and lowercase of the same character being checked by checkBitsA_z.
+   */
+  strcpy(string1, "NjnaM");
+  ok = hasUniqueChars(string1);
+  assert(ok);
+
+  /*
+   * Test that should fail, string with not unique characters but has multiple spaces being checked by checkBitsA_z and checkBitsexcl_amp.
+   */
+  ok = hasUniqueChars(string2);
+  assert(!ok);
+
+  /*
+   * Test that should pass, string with manny spaces, checked by checkBitsA_z.
+   */
+  strcpy(string1, "It has ben log dy");
+  ok = hasUniqueChars(string1);
+  assert(ok);
+
+  /*
+   * Test that should pass, edge case of ! checked by checkBitsA_z and checkBitsexcl_amp.
+   */
+  strcpy(string1, "Huge Win!");
+  ok = hasUniqueChars(string1);
+  assert(ok);
+
+  /*
+   * Test that should pass, edge case of @ checked by checkBitsA_z and checkBitsexcl_amp.
+   */
+  char string4[] = "@dan";
+  ok = hasUniqueChars(string4);
+  assert(ok);
+
+  /*
+   * Test that should escape, checked by checkInvalid() and has \n in the string.
+   */
   ok = hasUniqueChars(string3);
   assert(ok);
-  
-  // TODO: add your tests here
-
-
 
   // NOTE: if you add a \n to any tests, this should cause the
-  //       exit failure given. Keep this test last so you can check 
+  //       exit failure given. Keep this test last so you can check
   //       that others pass or fail as you expect them to.
-  strcpy(string3, "a b cc\n");
-  ok = hasUniqueChars(string3);
+  // strcpy(string3, "a b cc\n");
+  // ok = hasUniqueChars(string3);
   // other examples that should fail that you can place in strings
   // are \0 or \t or \r
   // See problem description for more details about escape sequences
-  
+
   return 0;
 }
